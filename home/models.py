3fa8a995af26986067dd1fe5174dea895e1ef391
nishanescode <nisha@nescode.com>
2018-12-27 15:42:59 +0530

    [Update]home

--- home/models.py
+++ home/models.py
@@ -6,21 +6,21 @@ from django.utils import timezone
 class Heropage(models.Model):
     herotitle = models.CharField(max_length=200)
     herotext = models.TextField()
-    button = models.URLField('/')
+    # button = models.URLField('/')
 
     def __str__(self):
         return self.herotitle
 
 
 class Process(models.Model):
-    image = models.ImageField(upload_to='', null=True, blank=True)
+    image = models.ImageField(upload_to='jumbotron2.html', null=True, blank=True)
     processtext = models.TextField()
 
 
 class Gyanpage(models.Model):
     gyantitle = models.CharField(max_length=200)
     gyantext = models.TextField()
-    button = models.URLField('/')
+    # button = models.URLField('/')
 
     def __str__(self):
         return self.gyantitle

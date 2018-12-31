8a800f401c59baf7bcd1c047e7c85a97d5621682
nishanescode <nisha@nescode.com>
2018-12-31 12:20:12 +0530

    [Update]sorese

--- sorese/urls.py
+++ sorese/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT,}),
+    ]

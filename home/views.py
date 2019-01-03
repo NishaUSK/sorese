325fbd6117000820079e85a5202253763b15b776
nishanescode <nisha@nescode.com>
2019-01-03 09:41:34 +0530

    [Update]home

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Process
 
 
 def base(request):
@@ -17,4 +19,5 @@ def jumbotron2(request):
     return render(request, 'home/jumbotron2.html', {})
 
 def jumbotron3(request):
-    return render(request, 'home/jumbotron3.html', {})
+    gyanpages = Gyanpage.objects.filter(gyantitle='title')
+    return render(request, 'home/jumbotron3.html', {'gyanpages': gyanpages})

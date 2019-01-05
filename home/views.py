e692fbc903ebca52082d42b967fd6196ef78ec02
nishanescode <nisha@nescode.com>
2019-01-05 10:43:38 +0530

    [Update]home

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Process
+from .models import Heropage
+from .models import Gyanpage
 
 
 def base(request):
@@ -13,11 +15,12 @@ def header(request):
     return render(request, 'home/header.html', {})
 
 def jumbotron1(request):
-    return render(request, 'home/jumbotron1.html', {})
+    heropages = Heropage.objects.filter(herotitle=timezone.now())
+    return render(request, 'home/jumbotron1.html', {'heropages': heropages})
 
 def jumbotron2(request):
     return render(request, 'home/jumbotron2.html', {})
 
 def jumbotron3(request):
-    gyanpages = Gyanpage.objects.filter(gyantitle='title')
+    gyanpages = Gyanpage.objects.filter(gyantitle='timezone.now()')
     return render(request, 'home/jumbotron3.html', {'gyanpages': gyanpages})
